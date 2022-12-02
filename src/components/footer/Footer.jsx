5dcf2ed9aefce11042e26a061cb12f4a9e913d54
Nitish Biswas <55139122+NitishBiswas@users.noreply.github.com>
2022-12-03 00:00:30 +0600

    Added footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import './footer.scss'
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <div className='footer'>
+            <div>Movie App</div>
+            <div>© 2022, Movie, Inc. or its affiliates</div>
+        </div>
     )
 }
 
