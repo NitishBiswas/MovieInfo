4eac34308f539e70641e9bab8a6bd5b22b440a81
Nitish Biswas <55139122+NitishBiswas@users.noreply.github.com>
2022-12-02 23:51:05 +0600

    Added header to this project

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,19 @@ import Footer from './components/footer/Footer'
 import Home from './components/home/Home'
 import MovieDetail from './components/movie-detail/MovieDetail'
 import PageNotFound from './components/page-not-found/PageNotFound'
+import './App.scss'
 
 const App = () => {
   return (
     <>
       <Header />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/movie/:imdbID' element={<MovieDetail />} />
-        <Route path='/*' element={<PageNotFound />} />
-      </Routes>
+      <div className="container">
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/movie/:imdbID' element={<MovieDetail />} />
+          <Route path='/*' element={<PageNotFound />} />
+        </Routes>
+      </div>
       <Footer />
     </>
   )
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import USER from '../../images/user.png'
+import './header.scss'
 
 const Header = () => {
+    const navigate = useNavigate();
     return (
-        <div>Header</div>
+        <div className='header'>
+            <div className="logo" onClick={() => navigate('/')}>Movie App</div>
+            <div className="user-image">
+                <img src={USER} alt="" />
+            </div>
+        </div>
     )
 }
 
